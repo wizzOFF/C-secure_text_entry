f6128b8fb8b6253addefdd3ec56c146fb429e1f5
WizzOFF <maher_off@proton.me>
2025-03-19 16:22:24 +0100

    secure_text_entry.c: retrieve a string > the principle of the fgets function

--- secure_text_entry.c
+++ secure_text_entry.c
@@ -1,13 +1,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
- 
+/*
 int main(int argc, char *argv[])
 {
     char nom[20] = {0};
  
     printf("Quel est votre nom ? ");
     scanf("%s", nom);
+    printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
+ 
+    return 0;
+}
+*/
+
+int main(int argc, char *argv[])
+{
+    char nom[10];
+ 
+    printf("Quel est votre nom ? ");
+    fgets(nom, 10, stdin);
     printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
  
     return 0;
