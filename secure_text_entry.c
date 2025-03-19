9bcb9b844b15b644275e2ea5a47966661a30a4cc
WizzOFF <maher_off@proton.me>
2025-03-19 16:12:45 +0100

    secure_text_entry.c: limits of the scanf function > string that contains spaces

---
+++ secure_text_entry.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(int argc, char *argv[])
+{
+    char nom[20] = {0};
+ 
+    printf("Quel est votre nom ? ");
+    scanf("%s", nom);
+    printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
+ 
+    return 0;
+}
